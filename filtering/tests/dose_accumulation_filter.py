3e71015f124363bc27b990b65cf7db155e9d6169
Oliver Kuster <ufexb@student.kit.edu>
2025-09-24 14:57:00 +0200

    Added gradient caclulation to the test.

--- filtering/tests/dose_accumulation_filter.py
+++ filtering/tests/dose_accumulation_filter.py
@@ -12,15 +12,25 @@ def test(seed):
     rho_0 = np.random.random((50, 50, 50))
     rho_0 = gaussian_filter(rho_0, 5)
     rho_0 = np.round(rho_0)
+    rho_0_torch = torch.tensor(rho_0, device='cuda', requires_grad=True)
     dose_filter = filter_loader("dose_conv", 10)
 
-    rho_filt = dose_filter(torch.tensor(rho_0, device='cuda'))
-    rho_filt = rho_filt > ConfigPrint.rho_th_GT
-    rho_filt = rho_filt.detach().cpu().numpy()
+    rho_filt = dose_filter(rho_0_torch)
+    rho_filt_np = rho_filt > ConfigPrint.rho_th_GT
+    rho_filt_np = rho_filt_np.detach().cpu().numpy()
 
     fig, (ax0, ax1) = plt.subplots(1, 2)
     ax0.imshow(rho_0[rho_0.shape[0]//2].T, origin='lower', cmap='binary')
-    ax1.imshow(rho_filt[rho_0.shape[0]//2].T, origin='lower', cmap='binary')
+    ax1.imshow(rho_filt_np[rho_0.shape[0]//2].T, origin='lower', cmap='binary')
+    plt.show()
+
+    sum = torch.sum(rho_filt)
+    sum.backward()
+    print(rho_0_torch.grad.sum())
+    grad = rho_0_torch.grad
+    grad_np = grad.detach().cpu().numpy()
+
+    plt.imshow(grad_np[grad_np.shape[0]//2].T, origin='lower')
     plt.show()
 
 
