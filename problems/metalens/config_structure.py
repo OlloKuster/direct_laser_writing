2d2b9e4fb9cfbca4b6c61c61dfaf0315cf63bfc3
Oliver Kuster <fy4301@kit.edu>
2025-09-25 09:38:04 +0200

    Added a simulation for the metalens and a test.

--- problems/metalens/config_structure.py
+++ problems/metalens/config_structure.py
@@ -1,14 +1,20 @@
 from dataclasses import dataclass
+import jax.numpy as jnp
 
 @dataclass
 class ConfigSim:
     """
     Config the 3D lens, defines the required parameters which are used for the simulation.
     """
-    resolution = 20
-    simulation_domain_shape = (11, 11, 12)
     rho_shape = (5, 5, 5)
-    currents_shape = (11, 11)
+    buffer_side = 1
+    buffer_top = 3
+    buffer_bottom = 1
+    dpml = 0.5
+    simulation_domain_shape = (int(jnp.ceil(dpml + buffer_side + 2*rho_shape[0] + buffer_side + dpml)),
+                               int(jnp.ceil(dpml + buffer_side + 2*rho_shape[1] + buffer_side + dpml)),
+                               int(jnp.ceil(dpml + buffer_bottom + rho_shape[2] + buffer_top + dpml)))
+    currents_shape = (simulation_domain_shape[0], simulation_domain_shape[1])
     sinks_shape = rho_shape
     wavelength = 1.55
     save_plot = True
@@ -17,9 +23,8 @@ class ConfigSim:
     kappa = (1e-5, 1)
     resize_factor = 1
 
-    dpml = 0.5
     location_currents = dpml + 1
-    location_focal_spot = 6*resolution
+    location_focal_spot = 6
 
     TARGET_EM = 20
     TARGET_MATERIAL = -1
