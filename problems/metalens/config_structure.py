e8ea62bf9e5d5683e07302ac0782f9a4f0bc933e
Oliver Kuster <ufexb@student.kit.edu>
2025-09-25 15:33:53 +0200

    changed the shape of rho a bit.

--- problems/metalens/config_structure.py
+++ problems/metalens/config_structure.py
@@ -6,7 +6,7 @@ class ConfigSim:
     """
     Config the 3D lens, defines the required parameters which are used for the simulation.
     """
-    rho_shape = (5, 5, 5)
+    rho_shape = (10, 10, 5)
     buffer_side = 1
     buffer_top = 3
     buffer_bottom = 1
