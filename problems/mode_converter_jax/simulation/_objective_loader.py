ee859c18619d8d2a9498c3e4317ab0de6edf8a1d
Oliver Kuster <ufexb@student.kit.edu>
2025-11-03 15:50:03 +0100

    Removed the mode converter in FDFD, will do it in FDTD

--- problems/mode_converter_jax/simulation/_objective_loader.py
+++
@@ -1,7 +0,0 @@
-from problems.mode_converter_jax.simulation.objective import objective_em_f
-
-
-def objective_loader(objective: str, *args):
-    if objective == "em_only":
-        currents, resolution, init_value = args
-        return objective_em_f(currents, resolution, init_value)
