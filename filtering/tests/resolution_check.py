d8e05f1d9c83343399f682eeeee8d85c0978d4df
Oliver Kuster <ufexb@student.kit.edu>
2025-11-24 14:08:12 +0100

    New resolution check.

--- filtering/tests/resolution_check.py
+++ filtering/tests/resolution_check.py
@@ -3,29 +3,65 @@ import scipy.ndimage
 import torch
 from scipy.ndimage import gaussian_filter
 import matplotlib.pyplot as plt
+import scipy
 
 from filtering._filter_loader import filter_loader
+from filtering.dose_model._dose_filter import dose_filter_f
 from filtering.dose_model.config_print import ConfigPrint
+from filtering.dose_model.utils_dose_sim import calc_laser_intensity
+from projection.tanh.subpixel_smoothed_projection import ssp_proj_jax_f
 
 
 def test(seed):
     np.random.seed(seed)
-    factors = [1, 2, 3, 4, 5]
-    result = []
-    resolution = 5
-    for factor in factors:
-        rho_0_init = np.random.random((5*resolution, 5*resolution, 5*resolution))
-        rho_0_g = gaussian_filter(rho_0_init, 2)
-        rho_0_scaled = scipy.ndimage.zoom(rho_0_g, factor)
-        rho_0 = np.round(rho_0_scaled)
-    # rho_0 = np.ones((5*resolution, 5*resolution, 5*resolution))
-        rho_0_torch = torch.tensor(rho_0, device='cuda', requires_grad=True)
-        dose_filter = filter_loader("dose_conv", resolution * factor)
+    # factors = [1, 2, 3, 4, 5]
+    # result = []
+    # resolution = 5
+    # for factor in factors:
+    #     rho_0_init = np.random.random((5*resolution, 5*resolution, 5*resolution))
+    #     rho_0_g = gaussian_filter(rho_0_init, 2)
+    #     rho_0_scaled = scipy.ndimage.zoom(rho_0_g, factor)
+    #     rho_0 = np.round(rho_0_scaled)
+    # # rho_0 = np.ones((5*resolution, 5*resolution, 5*resolution))
+    #     rho_0_torch = torch.tensor(rho_0, device='cuda', requires_grad=True)
+    #     dose_filter = filter_loader("dose_conv", resolution * factor)
+    #
+    #     rho_filt = dose_filter(rho_0_torch)
+    #     result.append(rho_filt.detach().cpu().numpy())
+    # plt.plot(factors, result)
+    # plt.show()
 
-        rho_filt = dose_filter(rho_0_torch)
-        result.append(rho_filt.detach().cpu().numpy())
-    plt.plot(factors, result)
+    resolutions = np.arange(10, 20, 1)
+    print(resolutions)
+    max = []
+    for resolution in resolutions:
+        resolution = int(resolution)
+        print(f"Resolution {resolution}")
+        dose_filter = dose_filter_f(resolution)
+
+        projection = ssp_proj_jax_f(ConfigPrint.rho_th_GT, np.inf, resolution)
+
+        rho_0_0 = np.random.rand(5*resolution, 5*resolution, 5*resolution)
+        rho_0 = np.round(scipy.ndimage.gaussian_filter(rho_0_0, sigma=resolution)) * 1.8*ConfigPrint.rho_th_GT
+        # rho_0 = np.ones_like(rho_0) * 1.8*ConfigPrint.rho_th_GT
+        rho = rho_0
+        rho = dose_filter(torch.tensor(rho, device='cuda', requires_grad=True))
+        #
+        rho = rho.detach().cpu().numpy().squeeze()
+        rho_proj = projection(rho)
+        f, ax = plt.subplots(1, 3)
+        ax[0].imshow(rho_0_0[rho_proj.shape[0]//2])
+        ax[1].imshow(rho_0[rho_proj.shape[0]//2])
+        ax[2].imshow(rho_proj[rho_proj.shape[0]//2])
+        plt.show()
+        max.append(np.max(rho))
+        print(f"Resolution {resolution}; Max {np.max(rho)}")
+
+    plt.plot(resolutions, max)
+    plt.ylim(0, 1)
+    plt.hlines(y=0.5, xmin=resolutions[0], xmax=resolutions[-1])
     plt.show()
+    # psf = psf.detach().cpu().numpy().squeeze()
 
 if __name__ == "__main__":
     test(42342)
