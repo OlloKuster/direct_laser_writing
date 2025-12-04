99a6862d663f51f2ecc85506a369bf7361b9f7e7
Oliver Kuster <ufexb@student.kit.edu>
2025-12-04 14:17:37 +0100

    Changed a few parameters, made the structure bigger

--- problems/metalens/simulation/config_structure.py
+++ problems/metalens/simulation/config_structure.py
@@ -7,10 +7,10 @@ class ConfigSim:
     """
     Config the 3D lens, defines the required parameters which are used for the simulation.
     """
-    rho_shape = (4, 4, 4)
+    rho_shape = (6, 6, 4)
     buffer_side = 1  # Buffer for the mask.
     buffer_top = 1  # Buffer for the mask.
-    space_top = 3  # Actual space above the design region.
+    space_top = 3.5  # Actual space above the design region.
     buffer_bottom = 1  # Thickness "substrate"
     dpml = 0.5  # Thickness PML
     simulation_domain_shape = (int(jnp.ceil(dpml + 2 * rho_shape[0] + dpml)),
@@ -24,7 +24,7 @@ class ConfigSim:
     resize_factor = 1  # Scales down the thermal simulation in case the FEM-mesh takes too much memory.
 
     location_currents = dpml + 1
-    location_focal_spot = dpml + buffer_bottom + rho_shape[2] + buffer_top + 1
+    location_focal_spot = dpml + buffer_bottom + rho_shape[2] + buffer_top + 2.
 
     TARGET_EM = 15  # Target EM Performance. Given in field enhancement at the focal spot
     TARGET_MATERIAL = 0.1  # Normalizes the target heat for the material. The initial value is 1-TARGET_MATERIAL.
