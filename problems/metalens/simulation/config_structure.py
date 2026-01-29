e3a897869ed97a4b75bcc01c59423c3fb356f331
Oliver Kuster <ufexb@student.kit.edu>
2026-01-29 15:13:44 +0100

    increased the size of the domain, moved the focal spot lower NA ~ 0.98

--- problems/metalens/simulation/config_structure.py
+++ problems/metalens/simulation/config_structure.py
@@ -7,7 +7,7 @@ class ConfigSim:
     """
     Config the 3D lens, defines the required parameters which are used for the simulation.
     """
-    rho_shape = (6, 6, 3)
+    rho_shape = (7.5, 7.5, 3)
     buffer_side = 1  # Buffer for the mask.
     buffer_top = 1  # Buffer for the mask.
     space_top = 3  # Actual space above the design region.
@@ -24,7 +24,7 @@ class ConfigSim:
     resize_factor = 1  # Scales down the thermal simulation in case the FEM-mesh takes too much memory.
 
     location_currents = dpml + 1
-    location_focal_spot = dpml + buffer_bottom + rho_shape[2] + buffer_top + 0.5
+    location_focal_spot = dpml + buffer_bottom + rho_shape[2] + buffer_top + 0.25
 
     TARGET_EM = 30  # Target EM Performance. Given in field enhancement at the focal spot
     TARGET_MATERIAL = 1.3  # Normalizes the target heat_eval for the material. The initial value is 1-TARGET_MATERIAL.
