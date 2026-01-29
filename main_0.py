e7c352f7a58c514304911f7444fd5d821681a3c5
Oliver Kuster <ufexb@student.kit.edu>
2026-01-29 15:14:54 +0100

    fully working setup ready for testing

--- main_0.py
+++ main_0.py
@@ -6,26 +6,18 @@ from dispenser import Dispenser
 from settings._setting_loader import setting_loader
 
 
-def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, load=None, eval=False, full_bin=False, run_id=0, device_id=0):
+def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, load=None, eval=False, full_bin=False, run_id=0):
     jax.config.update("jax_enable_x64", True)
-    torch.cuda.empty_cache()
-    jax_devices = jax.devices()
-    jax.default_device(jax_devices[device_id])
-    torch.set_default_device(f'cuda:{device_id}')
 
-    run = Dispenser.MODECONVERTER
+    run = Dispenser.LENS3D
     return run(resolution, betas, setting, loss_hist, em_loss_hist, opt=opt, load=load, eval=eval, full_bin=full_bin, run_id=run_id)
 
 
 if __name__ == "__main__":
-    init_setting = setting_loader("mode_converter", "normal_gauss")
-    setting = setting_loader("mode_converter", "dlw_regular")
+    setting = setting_loader("metalens", "dlw_regular")
     eval = True
-    device_id = 0
-    resolution = 8
+    resolution = 20
     loss_hist = []
     em_loss_hist = []
-    init_beta = [16]
     betas = [1, 16, 32, np.inf]
-    loss_hist, em_loss_hist = main(resolution, init_beta, init_setting, loss_hist, em_loss_hist, opt="optax", eval=eval, full_bin=False, run_id=0, device_id=0)
-    main(resolution, betas, setting, loss_hist, em_loss_hist, opt="optax", load=f"problems/mode_converter/plots/data_0_{init_beta[-1]}.h5", eval=eval, full_bin=False, run_id=1, device_id=1)
+    main(resolution, betas, setting, loss_hist, em_loss_hist, opt="nlopt", eval=eval, full_bin=True, run_id=1)
