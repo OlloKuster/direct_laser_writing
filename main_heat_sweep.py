042341cf66ee69b2970f400ce2caf0246dba8031
Oliver Kuster <ufexb@student.kit.edu>
2026-02-02 20:16:06 +0100

    Heat sweep

--- main_heat_sweep.py
+++ main_heat_sweep.py
@@ -6,27 +6,22 @@ from dispenser import Dispenser
 from settings._setting_loader import setting_loader
 
 
-def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, load=None, eval=False, full_bin=False, run_id=0, device_id=0):
+def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, max_evals, load=None, eval=False, full_bin=False, run_id=0):
     jax.config.update("jax_enable_x64", True)
-    torch.cuda.empty_cache()
-    jax_devices = jax.devices()
-    jax.default_device(jax_devices[device_id])
-    torch.set_default_device(f'cuda:{device_id}')
 
     run = Dispenser.LENS3D
-    return run(resolution, betas, setting, loss_hist, em_loss_hist, opt=opt, load=load, eval=eval, full_bin=full_bin, run_id=run_id)
+    return run(resolution, betas, setting, loss_hist, em_loss_hist, opt=opt, max_evals=max_evals, load=load, eval=eval, full_bin=full_bin, run_id=run_id)
 
 
 if __name__ == "__main__":
-    init_setting = setting_loader("metalens", "normal_gauss")
     setting = setting_loader("metalens", "dlw_regular")
     eval = False
     device_id = 0
     resolution = 10
     loss_hist = []
     em_loss_hist = []
-    init_beta = [16, 32, np.inf]
-    betas = [1, 16, 32, np.inf]
+    betas = np.linspace(0, 2, 15)
+    betas = np.append(betas, np.inf)
     run_id = 0
 
     target_material = [0.8, 1., 1.2, 1.4, 1.6]
@@ -35,8 +30,7 @@ if __name__ == "__main__":
         for void in target_void:
             loss_hist = []
             em_loss_hist = []
-            init_setting["target_material"] = mat
-            init_setting["target_void"] = void
-            main(resolution, init_beta, init_setting, loss_hist, em_loss_hist, opt="nlopt", eval=eval, full_bin=False, run_id=run_id, device_id=0)
+            setting["target_material"] = mat
+            setting["target_void"] = void
+            main(resolution, betas, setting, loss_hist, em_loss_hist, max_evals=10, opt="optax", eval=eval, full_bin=False, run_id=run_id)
             run_id = run_id + 1
-    # main(resolution, betas, setting, loss_hist, em_loss_hist, opt="optax", load=f"problems/metalens/plots/data_0_{init_beta[-1]}.h5", eval=eval, full_bin=False, run_id=2, device_id=1)
