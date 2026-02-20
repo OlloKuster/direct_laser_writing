c60892b3e2ea9c2143a1d14b0062935956295cf1
Oliver Kuster <ufexb@student.kit.edu>
2026-02-20 12:43:40 +0100

    Lens Run for HOREKA

--- main_0.py
+++ main_0.py
@@ -9,14 +9,14 @@ from settings._setting_loader import setting_loader
 def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, max_evals, load=None, eval=False, full_bin=False, run_id=0):
     jax.config.update("jax_enable_x64", True)
 
-    run = Dispenser.MODECONVERTER
+    run = Dispenser.LENS3D
     return run(resolution, betas, setting, loss_hist, em_loss_hist, opt=opt, max_evals=max_evals, load=load, eval=eval, full_bin=full_bin, run_id=run_id)
 
 
 if __name__ == "__main__":
-    setting = setting_loader("mode_converter", "dlw_regular")
+    setting = setting_loader("metalens", "dlw_regular")
     eval = True
-    resolution = 8
+    resolution = 20
     loss_hist = []
     em_loss_hist = []
     betas = np.logspace(0, 2, 20)
