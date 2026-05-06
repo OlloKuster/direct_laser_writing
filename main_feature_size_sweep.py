13ade969b638a032338a354c45727b556ad40c76
Oliver Kuster <ufexb@student.kit.edu>
2026-05-06 10:43:06 +0200

    Cleaned up the main_* files

--- main_feature_size_sweep.py
+++ main_feature_size_sweep.py
@@ -1,12 +1,26 @@
 import jax
-import torch
 import numpy as np
 
-from dispenser import Dispenser
 from settings._setting_loader import setting_loader
 
 
-def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, max_evals, load=None, eval=False, full_bin=False, run_id=0):
+def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, max_evals, load=None, eval=False, full_bin=False,
+         run_id=0):
+    """
+    This function can be run to reproduce the results of the paper regarding the minimum feature size.
+    :param resolution: Resolution of the problem (in px/lenght)
+    :param betas: Which binarization levels are used.
+    :param setting: Which setting is used for optimization.
+    :param loss_hist: List to track the Figure of Merit.
+    :param em_loss_hist: List to track the EM Figure of Merit.
+    :param opt: Which optimizer is used. Options are "nlopt" or "optax".
+    :param max_evals: How many evaluations/iterations are done in the optimization.
+    :param load: If set, will load the density from the specified file.
+    :param eval: If intermediate plots will be plotted.
+    :param full_bin: If the designs are fully binarized.
+    :param run_id: Tracking id for the runs.
+    :return: Loss and EM loss history.
+    """
     jax.config.update("jax_enable_x64", True)
 
     run = setting["run"]
@@ -36,4 +50,3 @@ if __name__ == "__main__":
                                        load=f'/scratch/local/okuster/Code/00_Main_Projects/dlw_params/problems/metalens/plots/data_base.h5')
 
         run_id = run_id + 1
-    # main(resolution, betas, setting, loss_hist, em_loss_hist, opt="optax", load=f"problems/metalens/plots/data_0_{init_beta[-1]}.h5", eval=eval, full_bin=False, run_id=2, device_id=1)
--- main_lp_weep.py
+++ main_lp_weep.py
@@ -1,12 +1,27 @@
 import jax
-import torch
 import numpy as np
 
 from dispenser import Dispenser
 from settings._setting_loader import setting_loader
 
 
-def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, max_evals, load=None, eval=False, full_bin=False, run_id=0):
+def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, max_evals, load=None, eval=False, full_bin=False,
+         run_id=0):
+    """
+    This function can be run to reproduce the results of the paper regarding the laser power sweep.
+    :param resolution: Resolution of the problem (in px/lenght)
+    :param betas: Which binarization levels are used.
+    :param setting: Which setting is used for optimization.
+    :param loss_hist: List to track the Figure of Merit.
+    :param em_loss_hist: List to track the EM Figure of Merit.
+    :param opt: Which optimizer is used. Options are "nlopt" or "optax".
+    :param max_evals: How many evaluations/iterations are done in the optimization.
+    :param load: If set, will load the density from the specified file.
+    :param eval: If intermediate plots will be plotted.
+    :param full_bin: If the designs are fully binarized.
+    :param run_id: Tracking id for the runs.
+    :return: Loss and EM loss history.
+    """
     jax.config.update("jax_enable_x64", True)
 
     run = Dispenser.LENS3D
