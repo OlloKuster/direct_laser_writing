Fixed the gradients

filtering/dose_model/DoseMSBPM.py
@@ -5,14 +5,14 @@ class DoseMSBPM(torch.nn.Module):
     """
     Base class for the dlw model.
     """
-    def __init__(self, rho_0_GT: torch.Tensor, intensity_nonlinear: torch.Tensor,
+    def __init__(self, rho_0_GT: torch.Tensor, psf: torch.Tensor,
                  print_params: list, nonlinearity: torch.Tensor,
                  device='cpu', dtype=torch.float64):
         """
 
         Args:
             rho_0: Initial structure/power lines [batch_size, 1, nx, ny, nz]
-            intensity_nonlinear: Point Spread Function intensity profile
+            psf: Point Spread Function intensity profile
             print_params: (sig_2_r, time_exposure, intensity_without_power, correction_factor) List the parameters
                             relevant to the print.
             nonlinearity: Power exponent of the nonlinearity.
@@ -20,7 +20,7 @@ class DoseMSBPM(torch.nn.Module):
             dtype: Datatype, recommended torch.float64
         """
         self.rho_0_GT = rho_0_GT.to(device).to(dtype)
-        self.intensity_nonlinear = intensity_nonlinear.to(device).to(dtype)
+        self.intensity_nonlinear = psf.to(device).to(dtype)
 
         self.nonlinearity = nonlinearity.to(device).to(dtype)
         self.factor_in_exp = print_params[0] * print_params[1] * (
@@ -42,7 +42,7 @@ class DoseMSBPMFull3D(DoseMSBPM):
         :param lp: Laser Power.
         :return: Accumulated power in the resist as a density.
         '''
-        conv = torch.nn.functional.conv3d(obj[None, None], self.intensity_nonlinear, padding='same')
+        conv = torch.nn.functional.conv3d(obj[None, None], torch.pow(self.intensity_nonlinear, 2), padding='same')
         rho = self.rho_0_GT * (1 - torch.exp(
             - self.factor_in_exp * self.correction_factor * conv * lp[:, :, None, None, None] ** self.nonlinearity))
 

main_0.py
@@ -29,7 +29,7 @@ def main(resolution, betas, setting, loss_hist, em_loss_hist, opt, max_evals, lo
 
 
 if __name__ == "__main__":
-    # setting_init = setting_loader("metalens", "no_filter")
+    setting_init = setting_loader("metalens", "no_filter")
     setting_final = setting_loader("frequency_filter", "dlw_em_only")
     eval = True
     resolution = 6
@@ -38,5 +38,7 @@ if __name__ == "__main__":
     betas_init = [8, 16]
     betas_final = [np.inf]
 
-    loss_hist, em_loss_hist = main(resolution, betas_init, setting_final, loss_hist, em_loss_hist, max_evals=50, opt="optax",
+    loss_hist, em_loss_hist = main(resolution, betas_init, setting_init, loss_hist, em_loss_hist, max_evals=20, opt="nlopt",
+                                   eval=eval, run_id=0)
+    loss_hist, em_loss_hist = main(resolution, betas_final, setting_final, loss_hist, em_loss_hist, max_evals=20, opt="nlopt",
                                    eval=eval, run_id=1)

optimizer/optimizer.py
@@ -43,7 +43,7 @@ def optimizer_nlopt(rho, objective, mask, filter, projection, init_projection, p
         @staticmethod
         def forward(ctx, x):
             grad_em_sim_f = jax.value_and_grad(objective, has_aux=True)
-            value_em_sim, grad_em_sim = grad_em_sim_f(projection(x.detach().cpu().numpy().astype(np.float64)))
+            value_em_sim, grad_em_sim = grad_em_sim_f(x.detach().cpu().numpy().astype(np.float64))
             em_hist.append(value_em_sim[1][0])
             print(f"transmission: {value_em_sim[1][0][0]}\treflection: {value_em_sim[1][0][1]}")
             cur_eps.append(value_em_sim[1][1])
@@ -160,7 +160,7 @@ def optimizer_optax(rho, objective, mask, filter, projection, init_projection, p
         @staticmethod
         def forward(ctx, x):
             grad_em_sim_f = jax.value_and_grad(objective, has_aux=True)
-            value_em_sim, grad_em_sim = grad_em_sim_f(projection(x.detach().cpu().numpy().astype(np.float64)))
+            value_em_sim, grad_em_sim = grad_em_sim_f(x.detach().cpu().numpy().astype(np.float64))
             em_hist.append(value_em_sim[1][0])
             cur_eps.append(value_em_sim[1][1])
             ctx.save_for_backward(torch.tensor(np.array(grad_em_sim), device='cuda', requires_grad=True))
@@ -177,7 +177,7 @@ def optimizer_optax(rho, objective, mask, filter, projection, init_projection, p
         rho_init = np.array(init_projection(rho) * mask)
         if mode == "jax":
             rho_final = filter(rho_init)
-            value_em_sim, grad_em_sim = jax.value_and_grad(objective, has_aux=True)(projection(rho_final))
+            value_em_sim, grad_em_sim = jax.value_and_grad(objective, has_aux=True)(rho_final)
             value = float(value_em_sim[0])  # Requires np float and not jax.numpy float
             value_em = float(value_em_sim[1][0])
             em_hist.append(value_em_sim[1][0])

problems/metalens/_run.py
@@ -73,7 +73,7 @@ def run(resolution, betas, setting: dict, loss_hist, em_loss_hist, opt, max_eval
     filter_0 = filter_loader(filters, filter_values, lp_deviation)
     projection_0 = projection_loader(projections, projection_values, betas[0], resolution)
 
-    objective_em = objective_loader(setting["init_em"], currents, resolution, 1, 1, 1)
+    objective_em = objective_loader(setting["init_em"], projection_0,  currents, resolution, 1, 1, 1)
     init_val_em, _ = objective_em(jnp.ones_like(rho_0))
     objective_heat = objective_loader(setting["init_heat"])
 
@@ -102,11 +102,11 @@ def run(resolution, betas, setting: dict, loss_hist, em_loss_hist, opt, max_eval
     for i in range(len(betas)):
         print(f"beta: {betas[i]}")
 
-        objective = objective_loader(objectives, currents, resolution, init_val_em, init_val_mat, init_val_void)
-
         filter = filter_loader(filters, filter_values, lp_deviation)
         projection = projection_loader(projections, projection_values, betas[i], resolution)
         init_projection = projection_loader(init_projections, init_projection_values, betas[i], resolution)
+
+        objective = objective_loader(objectives, projection, currents, resolution, init_val_em, init_val_mat, init_val_void)
 
         if opt == "optax":
             rho_0, loss, em_loss, grads = optimizer_optax(rho_0, objective, mask, filter, projection, init_projection,

problems/metalens/simulation/objective.py
@@ -7,7 +7,7 @@ from problems.metalens.simulation.simulation import em_simulation, heat_simulati
 from utility.helper import relu
 
 
-def objective_em_f(currents, resolution, init_value):
+def objective_em_f(projection, currents, resolution, init_value):
     """
     Generates the pure EM-objective function.
     :param currents: Source currents of the simulation.
@@ -22,7 +22,7 @@ def objective_em_f(currents, resolution, init_value):
         :param rho: Density (design variable) of the problem [0, 1].
         :return: Field enhancement at the focal spot of the x-component of E.
         """
-        E, eps = em_simulation(rho, currents, resolution)
+        E, eps = em_simulation(projection(rho), currents, resolution)
         focal_spot = E[0][
             E[0].shape[0] // 2, E[0].shape[1] // 2, int(jnp.ceil(ConfigSim.location_focal_spot * resolution))]
         return jnp.abs(focal_spot) / init_value, (jnp.abs(focal_spot) / init_value, eps)
@@ -48,7 +48,7 @@ def objective_heat_f():
     return objective_heat
 
 
-def objective_em_heat_f(currents, resolution, init_values):
+def objective_em_heat_f(projection, currents, resolution, init_values):
     """
     Generates the (softmax) connectivity-objective function.
     :param currents: Source currents of the simulation.
@@ -64,7 +64,7 @@ def objective_em_heat_f(currents, resolution, init_values):
         :param rho: Density (design variable) of the problem [0, 1].
         :return: Field enhancement at the focal spot of the x-component of E.
         """
-        E, eps = em_simulation(rho, currents, resolution)
+        E, eps = em_simulation(projection(rho), currents, resolution)
         focal_spot = E[0][
             E[0].shape[0] // 2, E[0].shape[1] // 2, int(jnp.ceil(ConfigSim.location_focal_spot * resolution))]
         return jnp.abs(focal_spot) / init_values[0], eps
@@ -75,7 +75,7 @@ def objective_em_heat_f(currents, resolution, init_values):
         :param rho: Density (design variable) of the problem [0, 1].
         :return: Tuple of material and void heat_eval.
         """
-        T_mat, T_void, _ = heat_simulation(rho, ConfigSim.resize_factor, resolution)
+        T_mat, T_void, _ = heat_simulation(projection(rho), ConfigSim.resize_factor, resolution)
         return T_mat, T_void
 
     def objective_softplus(rho_0):
@@ -110,7 +110,7 @@ def objective_em_heat_f(currents, resolution, init_values):
     return objective_softplus
 
 
-def objective_robust_em_heat_f(currents, resolution, init_values):
+def objective_robust_em_heat_f(projection, currents, resolution, init_values):
     """
     Generates the (softmax) connectivity-objective function. A dlw optimization is done by averaging over three
     different structures (eroded, normal and dialted).
@@ -127,7 +127,7 @@ def objective_robust_em_heat_f(currents, resolution, init_values):
         :param rho: Density (design variable) of the problem [0, 1].
         :return: Field enhancement at the focal spot of the x-component of E.
         """
-        E, eps = em_simulation(rho, currents, resolution)
+        E, eps = em_simulation(projection(rho), currents, resolution)
         focal_spot = E[0][
             E[0].shape[0] // 2, E[0].shape[1] // 2, int(jnp.ceil(ConfigSim.location_focal_spot * resolution))]
         return jnp.abs(focal_spot) / init_values[0], eps
@@ -138,7 +138,7 @@ def objective_robust_em_heat_f(currents, resolution, init_values):
         :param rho: Density (design variable) of the problem [0, 1].
         :return: Tuple of material and void heat_eval.
         """
-        T_mat, T_void, _ = heat_simulation(rho, ConfigSim.resize_factor, resolution)
+        T_mat, T_void, _ = heat_simulation(projection(rho), ConfigSim.resize_factor, resolution)
         return T_mat, T_void
 
     def objective_softplus(rho):